520f118ff567e8c31619166052fb4b06142337d4
Philipp Steinhaus <philipp_steinhaus@web.de>
2022-01-04 17:32:46 +0100

    skipped header and solved address commata problem

--- sparkScript.py
+++ sparkScript.py
@@ -9,6 +9,7 @@ from pyspark.sql.types import DoubleType
 from pyspark.sql.types import FloatType
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext, Row
+from itertools import islice
 
 #Setup and Cluster configuration
 
@@ -21,7 +22,30 @@ sqlContext = SQLContext(sc)
 
 #Reading in Data
 
+def cleanse_address(x):
+	result = re.search('(".*?")', x)
+	if result:
+		address = result.group(1)
+		new_address = address[1:-1].replace(",", ";") # remove the " and replace the commata
+		x = x.replace(address, new_address)
+	return x
+
 pollutionData = sc.textFile("Measurement_summary.csv", minPartitions=repartition_count)
+pollutionData = pollutionData.mapPartitionsWithIndex(lambda i, iter: islice(iter, 1, None) if i == 0 else iter)
+
+test = pollutionData.take(3)
+print()
+print(test)
+print()
+
+# replace all commata in address strings with semicolons
+pollutionData = pollutionData.map(lambda x: cleanse_address(x))
+
+test = pollutionData.take(3)
+print()
+print(test)
+print()
+
 pollutionData = pollutionData.map(lambda x: x.split(','))
 pollutionData = pollutionData.map(lambda x: (x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10])).persist()
 
